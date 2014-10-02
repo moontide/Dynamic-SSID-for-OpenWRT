2e5de247d131b89d29100e3382cc772fa9c97cd4
LiuYan 刘研 <lovetide@qq.com>
2014-10-02 17:13:44 +0800

    增加了 cnbeta 移动版新闻的获取以及 SSID 设置
    由于 UTF-8 字符存在被截断的情况，这里较好的处理了汉字被截断的情况，使得 SSID 内容长度最大化

--- README.md
+++ README.md
@@ -5,20 +5,28 @@ Dynamic-SSID-for-OpenWRT
 
 各个脚本从不同的源头采集信息，然后根据采集到的内容去改变 SSID，然后这些 SSID 名称就变成了信息广播的媒体了！
 
-我所用的路由器是 TP-Link 的 WR841N (硬件版本: v5/v6)，能够支持 8 个 SSID，除了正常使用的一个外，其他 7 个可用来广播信息 ~\_~
-举例：每天早晨从 http://kernel.org 获取最新的 linux 内核版本信息，并设置为 SSID 名称，以进行广播
+我所用的路由器是 TP-Link 的 WR841N (硬件版本: v5/v6)，能够支持 8 个 SSID，除了正常使用的一个外，其他 7 个可用来广播信息 ~\_~。举例：每天早晨从 http://distrowatch.com 获取最新的 linux 发行版本版本，并设置为 SSID 名称，以进行广播
 
-* SSID 2: `3.17-rc7 2014-09-28`
-* SSID 3: `3.16.3 2014-09-17`
-* SSID 4: `3.14.19 2014-09-17`
-* SSID 5: `3.12.29 2014-09-30`
-* SSID 6: `3.10.55 2014-09-17`
-* SSID 7: `3.4.104 2014-09-25`
-* SSID 8: `3.2.63 2014-09-13`
+* SSID 2: `10/01 Arch 2014.10.01`
+* SSID 3: `10/01 KNOPPIX 7.4.2`
+* SSID 4: `10/01 LXLE 14.04.1-rc3`
+* SSID 5: `10/01 LXLE 12.04.5-rc3`
+* SSID 6: `09/30 Plop 4.3.0-test1`
+* SSID 7: `09/30 Chakra 2014.09`
+* SSID 8: `09/30 IPCop 2.1.6-rc1`
 
 
 截图 | Screenshots
 ==================
 
-distrowatch-distributions.sh
-![DistroWatch latest distributions](https://github.com/moontide/Dynamic-SSID-for-OpenWRT/raw/master/screenshots/distrowatch-latest-distributions.png)
+## distrowatch-distributions.sh ##
+
+![DistroWatch 网站的最新发行版信息| DistroWatch latest distributions](https://github.com/moontide/Dynamic-SSID-for-OpenWRT/raw/master/screenshots/distrowatch-latest-distributions.png)
+
+## cnbeta-mobile ##
+
+这是测试在 SSID 中使用汉字的情况。
+
+通常来说，一个汉字的 UTF-8 编码后的字节长度为 3，所以，SSID 最大长度为 32 字节，最多就只能存放 10 个汉字 + 2 个英文字符了。 但为了应对中英文混杂的情况（中文字符少于 10 个字符，但整体多于 10 个字符），需要处理 UTF-8 字节序列被截断的情况。处理后，可以使得 SSID 内容长度最大化（若只取 10 个汉字，则还剩下 2 个字节未用； 若只取 10 个英文字符，则剩下 22 个字节未用）
+
+![cnbeta 手机版新闻 | cnbeta mobile news](https://github.com/moontide/Dynamic-SSID-for-OpenWRT/raw/master/screenshots/cnbeta-mobile-news.png)
---
+++ cnbeta-mobile-news.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+# BusyBox 里的 awk 貌似已经是以字节为单位的了 (也许跟 LANG 环境变量为空有关？)，
+# 如果是 gawk 的话，可能需要指定 -b / --characters-as-bytes 参数
+
+cnbeta=$(wget -O - http://m.cnbeta.com/wap | grep 'a href="/wap/view_' | awk -F= '
+BEGIN { init_char_int_index(); }
+
+function init_char_int_index()
+{
+	for (i=0; i<=255; i++)
+	{
+		t=sprintf ("%c", i);
+		_char_int_index[t]=i;
+	}
+}
+
+# 假设 UTF-8 字符串是被截断的，则删除被截断后的字符的剩余字节，以保证字符串的结尾不出现乱码
+function fix_utf8_truncation(s)
+{
+	len=split(s,a,"");
+	last_byte=_char_int_index[a[len]]; # 整个字符串的最后一个字节
+	if (debug) print "	last_byte:", a[len], and(last_byte,0xc0) > "/dev/stderr";
+	if (and(last_byte,0xC0)==0x80 || and(last_byte,0xC0)==0xC0) #检查 utf-8 多字节字符被截断的情况
+	{
+		# 先找到 utf-8 字节序列的起始字节
+		for (i=len; i>=len-6; i--)
+		{
+			if(and(_char_int_index[a[i]],0xC0)==0xC0)
+			{
+				first_utf8_byte=_char_int_index[a[i]];	# 最后一个 utf-8 字符的首个字节
+				if (debug) print "	在", i, "处发现 utf-8 起始字节", first_utf8_byte > "/dev/stderr"
+				break;
+			}
+		}
+
+		bytes=0;
+		# 计算该 utf-8 字符应该有多少个字节组成(虽然通常汉字由 3 字节组成，但这样更灵活、通用，避免遇到 4 个字节的字符导致结果不符的情况)
+		while (and(first_utf8_byte,0x80)==0x80) # utf-8 首字节高位有多少位 1，就应该有多少个字节
+		{
+			first_utf8_byte=lshift(first_utf8_byte,1);
+			bytes++;
+		}
+
+		# 然后判断 utf-8 首字节到字符串结尾的长度是否等于 utf-8 字符的完整字节长度，如果不是，则删除被截断的 utf-8 字符字节
+		if ((len-i+1) != bytes)
+		{
+			if (debug) print "	本 utf-8 字符长度为", bytes, "字节，被截断后只剩下了", (len-i+1), "字节，这些字节应该被删除" > "/dev/stderr"
+			s=substr(s, 1, i-1);
+			if (debug) print "修复结束后的字符串: [" s "]" > "/dev/stderr";
+		}
+	}
+	return s;
+}
+
+{
+	debug=1
+# 删除前面的 <div class="list"><a href="/wap/view_333623.htm">
+tmp=substr($0,50);
+
+i=index(tmp,"<")-1;
+tmp=substr(tmp, 1, 32);
+if (debug) print "[" tmp "]" > "/dev/stderr";
+
+tmp=fix_utf8_truncation(tmp);
+
+print tmp;
+}');
+
+#echo ------------------------
+#echo "$cnbeta"
+#echo ------------------------
+
+i=0
+IFS=$'\n'
+for n in $cnbeta
+do
+	i=$(expr $i + 1)
+
+	#echo "[$n]"
+	uci set wireless.@wifi-iface[$i].ssid="$n"
+
+	if [[ $i -ge 7 ]]
+	then
+		break;
+	fi
+done
+uci commit && wifi
